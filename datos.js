32b2b8e56aaa1bd0d0177f6208db5efff93b19f8
Ale Guerrero <103852141+AleGJ@users.noreply.github.com>
2026-07-11 22:21:07 +0200

    Update datos.js

--- datos.js
+++ datos.js
@@ -2,18 +2,18 @@ window.COMPETITION_DATA = {
   currentWeek: 1,
   participants: [
     { name: "Fercha", startWeight: 55.0 },
-    { name: "Ale", startWeight: 56.0 },
+    { name: "Ale Magna", startWeight: 56.0 },
     { name: "Alba", startWeight: 57.0 },
     { name: "Angie", startWeight: 58.0 },
-    { name: "Gabo", startWeight: 70.0 }
+    { name: "Pueblitos", startWeight: 70.0 }
   ],
   weeks: [
     { week: 1, results: [
       { name: "Fercha", weight: 54.5, activeKcal: 2100, activeDays: 5 },
-      { name: "Ale", weight: 54.7, activeKcal: 1850, activeDays: 6 },
+      { name: "Ale Magna", weight: 54.7, activeKcal: 1850, activeDays: 6 },
       { name: "Alba", weight: 55.8, activeKcal: 2400, activeDays: 7 },
       { name: "Angie", weight: 53.8, activeKcal: 1700, activeDays: 4 },
-      { name: "Gabo", weight: 68.9, activeKcal: 3200, activeDays: 3 }
+      { name: "Pueblitos", weight: 68.9, activeKcal: 3200, activeDays: 3 }
     ]},
     { week: 2, results: [] },
     { week: 3, results: [] },
