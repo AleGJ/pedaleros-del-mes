cf67937258a44ff2809fcd5e9f4d6f674a0bf79e
Ale Guerrero <103852141+AleGJ@users.noreply.github.com>
2026-07-12 17:30:38 +0200

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,19 +1,19 @@
 window.COMPETITION_DATA = {
   currentWeek: 1,
   participants: [
-    { name: "Fercha", startWeight: 55.0 },
-    { name: "Ale Magna", startWeight: 56.0 },
-    { name: "Alba", startWeight: 57.0 },
-    { name: "Angie", startWeight: 58.0 },
-    { name: "Pueblitos", startWeight: 70.0 }
+    { name: "China Capuchina", startWeight: 59.9 },
+    { name: "Ale Magna", startWeight: 54.9 },
+    { name: "Alba", startWeight: 58.6 },
+    { name: "Angie bonita", startWeight: 73.85 },
+    { name: "Gabo Momoa", startWeight: 124.7 }
   ],
   weeks: [
     { week: 1, results: [
-      { name: "Fercha", weight: 54.5, activeKcal: 2100, activeDays: 5 },
-      { name: "Ale Magna", weight: 54.7, activeKcal: 1850, activeDays: 6 },
-      { name: "Alba", weight: 55.8, activeKcal: 2400, activeDays: 7 },
-      { name: "Angie", weight: 53.8, activeKcal: 1700, activeDays: 4 },
-      { name: "Pueblitos", weight: 68.9, activeKcal: 3200, activeDays: 3 }
+      { name: "China Capuchina", weight: 59.9, activeKcal: 0, activeDays: 0 },
+      { name: "Ale Magna", weight: 54.9, activeKcal: 0, activeDays: 0 },
+      { name: "Alba", weight: 58.6, activeKcal: 2400, activeDays: 0 },
+      { name: "Angie bonita", weight: 73.85, activeKcal: 0, activeDays: 0 },
+      { name: "Gabo Momoa", weight: 124.7, activeKcal: 0, activeDays: 0 }
     ]},
     { week: 2, results: [] },
     { week: 3, results: [] },
