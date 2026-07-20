63af2303a0084069d26698f9ae079a754bc925b3
Ale Guerrero <103852141+AleGJ@users.noreply.github.com>
2026-07-20 09:56:02 +0200

    Update datos.js

--- datos.js
+++ datos.js
@@ -3,17 +3,17 @@ window.COMPETITION_DATA = {
   participants: [
     { name: "China Capuchina", startWeight: 59.9 },
     { name: "Ale Magna", startWeight: 54.9 },
-    { name: "Alba", startWeight: 58.6 },
+    { name: "Alba Wild", startWeight: 58.6 },
     { name: "Angie bonita", startWeight: 73.85 },
     { name: "Gabo Momoa", startWeight: 124.7 }
   ],
   weeks: [
     { week: 1, results: [
-      { name: "China Capuchina", weight: 0, activeKcal: 0, activeDays: 0 },
-      { name: "Ale Magna", weight: 0, activeKcal: 0, activeDays: 0 },
-      { name: "Alba", weight: 0, activeKcal: 0, activeDays: 0 },
-      { name: "Angie bonita", weight: 0, activeKcal: 0, activeDays: 0 },
-      { name: "Gabo Momoa", weight: 0, activeKcal: 0, activeDays: 0 }
+      { name: "China Capuchina", weight: 58.65, activeKcal: 0, activeDays: 0 },
+      { name: "Ale Magna", weight: 54.4, activeKcal: 2178, activeDays: 7 },
+      { name: "Alba Wild", weight: 58, activeKcal: 4162, activeDays: 7 },
+      { name: "Angie bonita", weight: 0, activeKcal: 3574, activeDays: 6 },
+      { name: "Gabo Momoa", weight: 126, activeKcal: 0, activeDays: 0 }
     ]},
     { week: 2, results: [] },
     { week: 3, results: [] },
