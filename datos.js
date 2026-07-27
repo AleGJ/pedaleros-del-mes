cabebc07d75bcad2b13d309b41d58d9f8b03d36c
Ale Guerrero <103852141+AleGJ@users.noreply.github.com>
2026-07-27 09:03:15 +0200

    Update datos.js

--- datos.js
+++ datos.js
@@ -12,10 +12,16 @@ window.COMPETITION_DATA = {
       { name: "China Capuchina", weight: 58.65, activeKcal: 2537, activeDays: 5 },
       { name: "Ale Magna", weight: 54.4, activeKcal: 2178, activeDays: 7 },
       { name: "Alba Wild", weight: 58, activeKcal: 4162, activeDays: 7 },
-      { name: "Angie bonita", weight: 0, activeKcal: 3574, activeDays: 6 },
+      { name: "Angie bonita", weight: 73.85, activeKcal: 3574, activeDays: 6 },
       { name: "Gabo Momoa", weight: 124, activeKcal: 4004, activeDays: 7 }
     ]},
-    { week: 2, results: [] },
+    { week: 2, results: [
+      { name: "China Capuchina", weight: 59.45, activeKcal: 4163, activeDays: 7 },
+      { name: "Ale Magna", weight: 54.8, activeKcal: 2985, activeDays: 7 },
+      { name: "Alba Wild", weight: 58.2, activeKcal: 3985, activeDays: 7 },
+      { name: "Angie bonita", weight: 72.1, activeKcal: 6280, activeDays: 7 },
+      { name: "Gabo Momoa", weight: 124.6, activeKcal: 4020, activeDays: 6 }   
+    ] },
     { week: 3, results: [] },
     { week: 4, results: [] }
   ]
